f0c6d596bce0688b1196a160c9d71919c02eecbc
GaiaRossi <gaia.rossi@edu.unife.it>
2024-08-30 18:52:52 +0200

    check_odte refactoring and minor changes

--- cyberphysical-application-operator.py
+++ cyberphysical-application-operator.py
@@ -14,6 +14,7 @@ def create_fn(spec, name, namespace, logger, **kwargs):
 
     if preferred_affinity == "":
         deployment_configs = deployments[0].get("configs")
+        deployment_affinity = deployments[0].get("affinity")
     
     else:
         for deployment in deployments:
@@ -23,11 +24,14 @@ def create_fn(spec, name, namespace, logger, **kwargs):
                 break
             
     for config in deployment_configs:
-        if config.get("kind") == "Deployment" and deployment_affinity is not None:
-            config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : deployments[0].get("affinity")}})
+        if config.get("kind") == "Deployment":
+            config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : f"{deployment_affinity}"}})
         kopf.label(config, {"createdFor": f"{name}"})
         kopf.adopt(config)
-        create_from_dict(k8s_client, config)
+        try:
+            create_from_dict(k8s_client, config)
+        except Exception as e:
+            logger.exception("Exception in object creation.")
   
 
 @kopf.on.delete('cyberphysicalapplications')
@@ -39,24 +43,25 @@ def delete_fn(spec, name, namespace, logger, **kwargs):
 def update_fn(body, **kwargs):
     pass
 
+
 @kopf.daemon('cyberphysicalapplications', cancellation_backoff=1.0, cancellation_timeout=3.0, initial_delay=5)
 async def check_odte(stopped, name, spec, namespace, body, logger, **kwargs):
     while not stopped:
         logger.info("Daemon listening...")
-        k8s_client = client.ApiClient()
+
         k8s_apps_v1 = client.AppsV1Api()
         k8s_core_v1 = client.CoreV1Api()
-        odte_threshold = float(body.get("spec").get("requirements").get("odte"))
-        deployments = body.get("spec").get("deployments")
-        current_depl = None
-        prometheus_url = None
 
+        odte_threshold = float(spec.get("requirements").get("odte"))
+        deployments = spec.get("deployments")
+
+        current_depl = None
         label_selector = f"createdFor={name}"
         try:
             resp = k8s_apps_v1.list_namespaced_deployment(namespace, label_selector=label_selector)
             current_depl = resp.items[0]
         except:
-            logger.info("No deployment found")
+            logger.info("No deployment found.")
             await stopped.wait(1)
             continue
 
@@ -67,29 +72,28 @@ async def check_odte(stopped, name, spec, namespace, body, logger, **kwargs):
         try:
             resp = requests.get(query_url)
         except:
-            logger.info("Prometheus not available")
+            logger.info("Prometheus not available.")
             await stopped.wait(1)
             continue
+        
         try:
             odte = float(json.loads(resp.text)["data"]["result"][0]["value"][1])
         except:
-            logger.info("ODTE not available")
+            logger.info("ODTE not available.")
             odte = None
             await stopped.wait(1)
             continue
 
         logger.debug(f"Last odte read: {odte}")
 
-        if odte and odte < odte_threshold:
-            logger.info("odte below threshold")
+        if odte is not None and odte < odte_threshold:
+            logger.debug("odte below threshold")
             if len(deployments) > 1:
-                next_depl = random.randint(0, len(deployments) - 1)
                 try:
                     resp = k8s_apps_v1.delete_namespaced_deployment(current_depl.metadata.name, namespace=current_depl.metadata.namespace)
                 except:
-                    pass
+                    logger.exception("Exception deleting deployment.")
 
-                # attendo terminazione pod
                 terminated = False
                 label_selector = f'app={app_name}'
                 while not terminated:
@@ -98,20 +102,24 @@ async def check_odte(stopped, name, spec, namespace, body, logger, **kwargs):
                         if len(resp.items) == 0:
                             terminated = True
                     except:
-                        logger.info("Cannot list pods")
+                        logger.info("Cannot list pods.")
                 
 
+                next_depl = random.randint(0, len(deployments) - 1)
                 deployment = deployments[next_depl]
                 deployment_configs = deployment.get("configs") 
+                deployment_affinity = deployment.get("affinity")
+
                 for config in deployment_configs:
                     if config.get("kind") == "Deployment":
-                        config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : deployment.get("affinity")}})
+                        config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : f"{deployment_affinity}"}})
+                        kopf.adopt(config)
                         kopf.label(config, {"createdFor": f"{name}"})
                         try:
-                            create_from_dict(k8s_client, config)
+                            resp = k8s_apps_v1.create_namespaced_deployment(namespace, config)
                             logger.info(f"Deployment replaced.")
                         except:
-                            logger.info("Exception creating replace deployment")
+                            logger.exception("Exception creating replace deployment.")
         
 
         await stopped.wait(1)

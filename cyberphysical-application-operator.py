6a3e5c14ee4a72de233aaad66bd58438c59f4fd1
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-09-09 14:06:17 +0200

    deleting all objects relating to current deployment when migrating

--- cyberphysical-application-operator.py
+++ cyberphysical-application-operator.py
@@ -1,6 +1,102 @@
-import kopf, requests, json, random
+import kopf, requests, json, random, re
 from kubernetes import client
 from kubernetes.utils import create_from_dict
+
+UPPER_FOLLOWED_BY_LOWER_RE = re.compile('(.)([A-Z][a-z]+)')
+UPPER_FOLLOWED_BY_LOWER_RE = re.compile('(.)([A-Z][a-z]+)')
+LOWER_OR_NUM_FOLLOWED_BY_UPPER_RE = re.compile('([a-z0-9])([A-Z])')
+LOWER_OR_NUM_FOLLOWED_BY_UPPER_RE = re.compile('([a-z0-9])([A-Z])')
+
+
+def delete_from_dict(k8s_client, data, verbose=False, namespace='default',
+                     **kwargs):
+    # If it is a list type, will need to iterate its items
+    api_exceptions = []
+    k8s_objects = []
+
+    if "List" in data["kind"]:
+        # Could be "List" or "Pod/Service/...List"
+        # This is a list type. iterate within its items
+        kind = data["kind"].replace("List", "")
+        for yml_object in data["items"]:
+            # Mitigate cases when server returns a xxxList object
+            # See kubernetes-client/python#586
+            if kind != "":
+                yml_object["apiVersion"] = data["apiVersion"]
+                yml_object["kind"] = kind
+            try:
+                deleted = delete_from_yaml_single_item(
+                    k8s_client, yml_object, verbose, namespace=namespace,
+                    **kwargs)
+                k8s_objects.append(deleted)
+            except client.rest.ApiException as api_exception:
+                api_exceptions.append(api_exception)
+    else:
+        # This is a single object. Call the single item method
+        try:
+            deleted = delete_from_yaml_single_item(
+                k8s_client, data, verbose, namespace=namespace, **kwargs)
+            k8s_objects.append(deleted)
+        except client.rest.ApiException as api_exception:
+            api_exceptions.append(api_exception)
+
+    # In case we have exceptions waiting for us, raise them
+    if api_exceptions:
+        raise FailToDeleteError(api_exceptions)
+
+    return k8s_objects
+
+def delete_from_yaml_single_item(
+        k8s_client, yml_object, verbose=False, **kwargs):
+    group, _, version = yml_object["apiVersion"].partition("/")
+    if version == "":
+        version = group
+        group = "core"
+    # Take care for the case e.g. api_type is "apiextensions.k8s.io"
+    # Only replace the last instance
+    group = "".join(group.rsplit(".k8s.io", 1))
+    # convert group name from DNS subdomain format to
+    # python class name convention
+    group = "".join(word.capitalize() for word in group.split('.'))
+    fcn_to_call = "{0}{1}Api".format(group, version.capitalize())
+    k8s_api = getattr(client, fcn_to_call)(k8s_client)
+    # Replace CamelCased action_type into snake_case
+    kind = yml_object["kind"]
+    kind = UPPER_FOLLOWED_BY_LOWER_RE.sub(r'\1_\2', kind)
+    kind = LOWER_OR_NUM_FOLLOWED_BY_UPPER_RE.sub(r'\1_\2', kind).lower()
+    # Expect the user to create namespaced objects more often
+    if hasattr(k8s_api, "delete_namespaced_{0}".format(kind)):
+        # Decide which namespace we are going to put the object in,
+        # if any
+        if "namespace" in yml_object["metadata"]:
+            namespace = yml_object["metadata"]["namespace"]
+            kwargs['namespace'] = namespace
+        if "name" in yml_object["metadata"]:
+            name = yml_object["metadata"]["name"]
+            kwargs['name'] = name
+        resp = getattr(k8s_api, "delete_namespaced_{0}".format(kind))(**kwargs)
+    else:
+        kwargs.pop('namespace', None)
+        kwargs.pop('name', None)
+        resp = getattr(k8s_api, "delete_{0}".format(kind))(**kwargs)
+    if verbose:
+        msg = "{0} deleted.".format(kind)
+        if hasattr(resp, 'status'):
+            msg += " status='{0}'".format(str(resp.status))
+        print(msg)
+    return resp
+
+class FailToDeleteError(Exception):
+    def __init__(self, api_exceptions):
+        self.api_exceptions = api_exceptions
+
+    def __str__(self):
+        msg = ""
+        for api_exception in self.api_exceptions:
+            msg += "Error from server ({0}): {1}".format(
+                api_exception.reason, api_exception.body)
+        return msg
+
 
 @kopf.on.create("cyberphysicalapplications")
 def create_fn(spec, name, logger, **kwargs):
@@ -11,7 +107,6 @@ def create_fn(spec, name, logger, **kwargs):
 
     deployment_affinity = None
     deployment_configs = None
-    deployment_name = None
     deployment_namespace = None
     deployment_app_name = None
     deployment_prometheus_url = None
@@ -30,7 +125,6 @@ def create_fn(spec, name, logger, **kwargs):
     for config in deployment_configs:
         if config.get("kind") == "Deployment":
             config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : f"{deployment_affinity}"}})
-            deployment_name = config.get("metadata").get("name")
             deployment_namespace = config.get("metadata").get("namespace")
             deployment_app_name = config.get("metadata").get("labels").get("app")
             deployment_prometheus_url = config.get("spec").get("template").get("metadata").get("annotations").get("prometheusUrl")
@@ -42,10 +136,10 @@ def create_fn(spec, name, logger, **kwargs):
             logger.exception("Exception in object creation.")
 
     return {
-        "child-deployment-name": deployment_name,
         "child-deployment-namespace": deployment_namespace,
         "child-deployment-app-name": deployment_app_name,
-        "child-deployment-prometheus-url": deployment_prometheus_url
+        "child-deployment-prometheus-url": deployment_prometheus_url,
+        "child-deployment-affinity": deployment_affinity
     }
 
 def get_prometheus_odte(prometheus_url, app_name, logger):
@@ -71,13 +165,6 @@ def choose_next_deployment(deployments):
     next_depl_index = random.randint(0, len(deployments) - 1)
     return deployments[next_depl_index]
 
-def delete_current_deployment(k8s_apps_v1, depl_name, depl_namespace, logger):
-    try:
-        k8s_apps_v1.delete_namespaced_deployment(depl_name, depl_namespace)
-    except:
-        logger.exception("Exception deleting deployment.")
-        raise Exception
-
 def ensure_pod_termination(k8s_core_v1, app_name, namespace, logger):
     terminated = False
     label_selector = f'app={app_name}'
@@ -90,12 +177,12 @@ def ensure_pod_termination(k8s_core_v1, app_name, namespace, logger):
             logger.info("Cannot list pods.")
 
 @kopf.daemon('cyberphysicalapplications', cancellation_backoff=1.0, cancellation_timeout=3.0, initial_delay=5)
-async def check_odte(stopped, name, spec, status, namespace, logger, **kwargs):
+async def check_odte(stopped, name, spec, status, logger, **kwargs):
     exception_delay = 2
     loop_delay = 1
     replacement_delay = 4
 
-    k8s_apps_v1 = client.AppsV1Api()
+    k8s_client = client.ApiClient()
     k8s_core_v1 = client.CoreV1Api()
     
     while not stopped:
@@ -104,10 +191,10 @@ async def check_odte(stopped, name, spec, status, namespace, logger, **kwargs):
         odte_threshold = float(spec.get("requirements").get("odte"))
         deployments = spec.get("deployments")
 
-        child_deployment_name = status.get("create_fn").get("child-deployment-name")
         child_deployment_namespace = status.get("create_fn").get("child-deployment-namespace")
         child_deployment_app_name = status.get("create_fn").get("child-deployment-app-name")
         child_deployment_prometheus_url = status.get("create_fn").get("child-deployment-prometheus-url")
+        current_deployment_affinity = status.get("create_fn").get("child-deployment-affinity")
 
         try:
             odte = get_prometheus_odte(child_deployment_prometheus_url, child_deployment_app_name, logger)
@@ -119,12 +206,17 @@ async def check_odte(stopped, name, spec, status, namespace, logger, **kwargs):
         if odte is not None and odte < odte_threshold:
             logger.debug("odte below threshold")
             if len(deployments) > 1:
-                
+
                 try:
-                    delete_current_deployment(k8s_apps_v1, child_deployment_name, child_deployment_namespace, logger)
+                    for deployment in deployments:
+                        if current_deployment_affinity == deployment.get("affinity"):
+                            configs = deployment.get("configs")
+                            for config in configs:
+                                delete_from_dict(k8s_client, config)
+
                     ensure_pod_termination(k8s_core_v1, child_deployment_app_name, child_deployment_namespace, logger)
                 except:
-                    raise kopf.TemporaryError("Exception deleting current deployment", delay=exception_delay)
+                    raise kopf.TemporaryError("Exception deleting objects", delay=exception_delay)
                  
                 next_deployment = choose_next_deployment(deployments)
                 next_deployment_configs = next_deployment.get("configs") 
@@ -132,24 +224,22 @@ async def check_odte(stopped, name, spec, status, namespace, logger, **kwargs):
 
                 for config in next_deployment_configs:
                     if config.get("kind") == "Deployment":
-                        config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : f"{next_deployment_affinity}"}})
-                        kopf.adopt(config)
-                        kopf.label(config, {"related-to": f"{name}"})
-                        
+                        config["spec"]["template"]["spec"].update({"nodeSelector": {"zone" : f"{next_deployment_affinity}"}})                        
                         next_deployment_namespace = config.get("metadata").get("namespace")
-
-                        status["create_fn"]["child-deployment-name"] = config.get("metadata").get("name")
                         status["create_fn"]["child-deployment-namespace"] = next_deployment_namespace
                         status["create_fn"]["child-deployment-app-name"] = config.get("metadata").get("labels").get("app")
                         status["create_fn"]["child-deployment-prometheus-url"] = config.get("spec").get("template").get("metadata").get("annotations").get("prometheusUrl")
-                        
-                        try:
-                            k8s_apps_v1.create_namespaced_deployment(next_deployment_namespace, config)
-                            logger.info(f"Deployment replaced.")
-                            await stopped.wait(replacement_delay)
-                            continue
-                        except:
-                            logger.exception("Exception creating replacement deployment.")
+                        status["create_fn"]["child-deployment-affinity"] = next_deployment_affinity
+                    
+                    kopf.adopt(config)
+                    kopf.label(config, {"related-to": f"{name}"})
+                    try:
+                        create_from_dict(k8s_client, config)
+                    except:
+                        logger.exception("Exception creating replacement object.")
+
+                logger.info(f"Replaced.")
+                await stopped.wait(replacement_delay)
 
             else:
                 logger.warn("No other deployment to switch to.")        
